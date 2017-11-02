54462de44a625db2403ed3c0864a343fb0cd40a7
codestk <codestk@gmail.com>
2017-11-02 22:30:22 +0700

    ดดด
    ดดด

---
+++ Deploy.bat
@@ -0,0 +1,13 @@
+
+ 
+
+
+copy D:\BBL\CreditCardForm\CreditCardForm\bin\CreditCardForm.dll  C:\Inetpub\wwwroot\sc82u2\Website\bin /y
+copy D:\BBL\CreditCardForm\CreditCardForm\bin\CreditCardForm.pdb  C:\Inetpub\wwwroot\sc82u2\Website\bin /y
+copy D:\Azure\BBLUtility\bin\Debug\BBLFormUtility.dll  C:\Inetpub\wwwroot\sc82u2\Website\bin /y
+copy D:\Azure\BBLUtility\bin\Debug\BBLFormUtility.pdb  C:\Inetpub\wwwroot\sc82u2\Website\bin /y
+ 
+start "" http://sc82u2.local
+
+
+REM  $(ProjectDir)\Deploy.bat
